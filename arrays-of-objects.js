2a526ae68a9f76d99a902a7418d02eb9b0abf8f4
Janet Leon <leon.irenejanet@gmail.com>
2018-08-22 11:33:42 -0700

    completed Properties that aren't there

--- arrays-of-objects.js
+++ arrays-of-objects.js
@@ -1,11 +1,18 @@
+'use strict';
 const arrayOfObjects = [
-  { name: 'Gandalf', jobTitle: 'Wizard' },
-  { name: 'Saruman', jobTitle: 'Big Wizard' },
-  { name: 'Frodo', jobTitle: 'Ring-bearer' },
-  { name: 'Faramir', jobTitle: 'Captain of Gondor' },
+  { name: 'Gandalf', jobTitle: 'Wizard' , boss: 'Saruman'},
+  { name: 'Saruman', jobTitle: 'Big Wizard', boss: 'Sauron' },
+  { name: 'Frodo', jobTitle: 'Ring-bearer' , boss: 'Sauron'},
+  { name: 'Faramir', jobTitle: 'Captain of Gondor', boss: 'Aragorn' },
   { name: 'Aragorn', jobTitle: 'The one true rightful King of Gondor' }
 ];
 
-arrayOfObjects.forEach(person =>
-  console.log(`Name: ${person.name} Job title: ${person.jobTitle}`)
-);
+arrayOfObjects.forEach(person =>{
+  if (person.hasOwnProperty('boss')) {
+  console.log(`${person.jobTitle} ${person.name} reports to ${person.boss}. `);
+} else {
+  console.log(`${person.jobTitle} ${person.name} doesn't report to anybody. `);
+}
+});
+
+
