a964b0ca07a8c733095afbd2e50c98625b46f6fe
Gabriel Morris <gabemorris@outlook.com>
2018-08-22 15:58:33 -0700

    Working findOne function using single for loop

--- database-search.js
+++ database-search.js
@@ -7,38 +7,21 @@ const HEROES = [
   { id: 6, name: 'Aquaman', squad: 'Justice League' },
   { id: 7, name: 'Hulk', squad: 'Avengers' }
 ];
-const findOne = function(arr, query) {
-  // See for-of loops - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of
-  // Iterates through array placing current element in locally scoped variable `hero`
-  for (const hero of arr) {
-    console.log(hero);
-    let assumeMatch = true;
-
-    // loop through each key in `query` object
-    for (const key in query) {
-      // when first value in query[key] does NOT match hero[key]
-      // break out of the loop and set assumeMatch = false
-      if (query[key] !== hero[key]) {
-        assumeMatch = false;
-        break;
-      }
-    }
-
-    // if failed to match, continue to next hero or exit loop if at last hero
-    if (!assumeMatch) continue;
-
-    // otherwise a match was found! return the hero
-    return hero;
+function findOne(arr, query) {
+  for (let hero of arr) {
+    if (
+      Object.keys(query).every(
+        key => hero.hasOwnProperty(key) && hero[key] === query[key]
+      )
+    )
+      return hero;
   }
-
-  // heroes array was exited without a match so return null
   return null;
-};
+}
 // console.log(
-//   findOne(HEROES, { id: 1, name: 'Captain America', squad: 'Avengers' })
-// );
+//   findOne(HEROES,  id: 1, name: 'Captain America', squad: 'Avengers' })
 console.log(findOne(HEROES, { id: 1 }));
-// => { id: 1, name: 'Captain America', squad: 'Avengers' }
+// => { id: 1, name: 'Captain America'uad: 'Avengers' }
 
 console.log(findOne(HEROES, { id: 10 }));
 // => null
