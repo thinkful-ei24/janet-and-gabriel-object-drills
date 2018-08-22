8d87e4517a40e6a31a3ab6d3ab849a04cf183ac1
Gabriel Morris <gabemorris@outlook.com>
2018-08-22 08:43:43 -0700

    Object updater function

--- object-creator.js
+++ object-creator.js
@@ -2,25 +2,24 @@
 
 function createMyObject() {
   // your code here
-    return {
-        foo: 'bar',
-        answerToUniverse: 42,
-        'olly olly': 'oxen free',
-        sayHello: function(){ return 'hello';}
-
-    };
-
-
+  return {
+    foo: 'bar',
+    answerToUniverse: 42,
+    'olly olly': 'oxen free',
+    sayHello: function() {
+      return 'hello';
+    }
+  };
 }
-  
-/* From here down, you are not expected to 
-     understand.... for now :)  
-     
-     
+
+/* From here down, you are not expected to
+     understand.... for now :)
+
+
      Nothing to see here!
-     
+
   */
-  
+
 (function testCreateMyObject() {
   var obj = createMyObject();
   if (typeof obj !== 'object') {
@@ -35,29 +34,28 @@ function createMyObject() {
     }
   });
   if (obj.foo !== 'bar') {
-    console.error("ERROR: Value for `foo` should be 'bar' but was " + obj.foo);
+    console.error('ERROR: Value for `foo` should be \'bar\' but was ' + obj.foo);
     return false;
   }
   if (obj.answerToUniverse !== 42) {
     console.error(
       'ERROR: Value for `answerToUniverse` should be 42 but was ' +
-          obj.answerToUniverse
+        obj.answerToUniverse
     );
     return false;
   }
   if (obj['olly olly'] !== 'oxen free') {
     console.error(
-      "ERROR: Value for `'olly olly'` should be 'oxen free' but was " +
-          obj['olly olly']
+      'ERROR: Value for `\'olly olly\'` should be \'oxen free\' but was ' +
+        obj['olly olly']
     );
     return false;
   }
   if (!(typeof obj.sayHello === 'function' && obj.sayHello() === 'hello')) {
     console.error(
-      "ERROR: Value for `sayHello` must be a function that returns the string 'hello'"
+      'ERROR: Value for `sayHello` must be a function that returns the string \'hello\''
     );
     return false;
   }
   console.log('SUCCESS: Your function works!');
 })();
-  
